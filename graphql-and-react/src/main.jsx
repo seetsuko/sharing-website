8a35ee9859289ecd7110a2b5752866856934dc01
seetsuko <radgjms@gmail.com>
2023-08-20 11:42:29 +0900

    apollo-clientをploviderで全体に使えるようにした

--- graphql-and-react/src/main.jsx
+++ graphql-and-react/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: "https://api-ap-northeast-1.hygraph.com/v2/cllhi11c20ucx01ut6d79gr7e/master",
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
